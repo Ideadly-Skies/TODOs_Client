37ab6085caa93d361da4054d7139625f6449782e
IdeadlySkies <Obie.kal22@gmail.com>
2025-06-28 19:24:15 +0700

    fix: wrong filepath for TODOList in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 
 import Header from './components/Header';
 import Form from './components/Form';
-import TODOList from './components/TodoList';
+import TODOList from './components/TODOList';
 import TODOHero from './components/TODOHero';
 import { useState, useEffect } from 'react';
 
